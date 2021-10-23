18c557634cde59a2dd749de1b6573f10de210e93
Rithik Samanthula <rithikdeveloper754@gmail.com>
2021-10-23 20:09:04 +0530

    Add files via upload

---
+++ src/code2rithik/github/io/Main.java
@@ -0,0 +1,43 @@
+package code2rithik.github.io;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    int myIntValue = 10;
+	    int anotherIntValue = myIntValue;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("anotherIntValue = " + anotherIntValue);
+
+        anotherIntValue++;
+
+        System.out.println("myIntValue = " + myIntValue);
+        System.out.println("anotherIntValue = " + anotherIntValue);
+
+        int[] myIntArray = new int[5];
+        int[] anotherArray = myIntArray;
+
+        System.out.println("myIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("anotherArray= " + Arrays.toString(anotherArray));
+
+        anotherArray[0] = 1;
+
+        System.out.println("After change myIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("After change   anotherArray= " + Arrays.toString(anotherArray));
+
+        anotherArray = new int[] {4, 5, 6, 7, 8};
+        modifyArray(myIntArray);
+
+        System.out.println("After modify myIntArray = " + Arrays.toString(myIntArray));
+        System.out.println("After modify anotherArray= " + Arrays.toString(anotherArray));
+
+    }
+
+    private static void modifyArray(int[] array) {
+
+        array [0] = 2;
+        array = new int[] {1, 2, 3, 4, 5};
+    }
+}
